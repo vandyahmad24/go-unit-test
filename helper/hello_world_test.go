4c2d60bea2ec3010c1de0855cea5db298537f69a
vandy ahmad <vandyahmad2404@gmail.com>
2021-09-03 21:58:20 +0700

    bencmark map

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -7,16 +7,41 @@ import (
 	"testing"
 )
 
+
+//benchmart
+func BenchmarkHelloWorld(b *testing.B){
+	for i:=0; i<b.N; i++{
+		HelloWorld("vandy")
+	}
+}
+//mapping benchmartk
+func BenchmarkTable(b *testing.B){
+	benchmart := []struct{
+		name string
+		request string
+	}{
+		{"Vandy","Vandy"},{"Ahmad","Ahmad"},{"Misry","Misry"},{"Ar Razy","Ar Razy"},
+	}
+	for _, v := range benchmart{
+		b.Run(v.name, func(b *testing.B) {
+			for i:=1; i<b.N; i++{
+				HelloWorld(v.request)
+			}
+		})
+	}
+}
+
+
 //untuk membuat file unit test hariss diakhir _test
 //hello_world_test.go
 //Aturan Function Harus diawali Test
-
 func TestMain(m *testing.M){
 	//sebelum unit test
 	fmt.Println("Sebelum unit test")
 	m.Run()
 	fmt.Println("Setelah unit test")
 }
+//mapping
 
 //unit sub test
 func TestSubTest(t *testing.T){
@@ -25,7 +50,7 @@ func TestSubTest(t *testing.T){
 		require.Equal(t, "Hello misry",result, "Hasil Harus Hello misry")
 	})
 	t.Run("Ahmad", func(t *testing.T) {
-		result := HelloWorld("ar raszy")
+		result := HelloWorld("ar razy")
 		require.Equal(t, "Hello ar razy",result, "Hasil Harus Hello ar razy")
 	})
 }
